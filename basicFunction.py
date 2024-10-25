6c9eafdd74cfcd5ca3f617428eb8a230f0894247
MakWigglz <wigglz734@gmail.com>
2024-10-25 23:56:03 +0300

    Create basicFunction.py
    A Pull request from a new Branch...

---
+++ basicFunction.py
@@ -0,0 +1,15 @@
+# structure of a function in Python:
+
+def add_four(num1, num2, num3, num4):
+    sum_three = num1 + num2 + num3 + num4
+    return sum_three
+
+
+
+# what is a Parameter and what is an Argument?
+# def is the built-in keyword in Python that is used to declare functions.
+# add_four is the name of the function.
+# (num1, num2, num3, num4) is the list of parameters needed for the function.
+# : indicates the start of the function body.
+# sum_three = num1 + num2 + num3’ is the code in the function body followed by the indentation.
+# return is the built-in keyword that exits the function and returns the output sum_three.
